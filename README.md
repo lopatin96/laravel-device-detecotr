6a04f0199d39f73bcc102c211c4225b479798936
vladyslavlopatin <ilopatin3@gmail.com>
2024-10-30 01:19:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install
+### Migrations
+Run migrations:
+```php
+php artisan migrate
+```
+
+# Publishing
+### Migrations
+```php
+php artisan vendor:publish --tag="laravel-device-detector-migrations"
+```
---
+++ src/DeviceDetectorProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Atin\LaravelDeviceDetector;
+
+use Atin\LaravelDeviceDetector\Providers\EventServiceProvider;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use hisorange\BrowserDetect\Facade as Browser;
+
+
+class DeviceDetectorProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        \Event::listen(Registered::class, static function ($event) {
+            $event->user->forceFill([
+                'device' => Browser::deviceType(),
+            ])->save();
+
+
+        });
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('/migrations')
+        ], 'laravel-lang-switcher-migrations');
+    }
+}
